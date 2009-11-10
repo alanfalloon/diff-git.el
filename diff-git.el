c9dfe169dcc4da01c8e2f7c2e3b7fd213c0f3f5d
Alan Falloon <alan.falloon@gmail.com>
2009-11-10 01:06:14 -0500

    Expanded Commentary section and copied it as README

--- diff-git.el
+++ diff-git.el
@@ -22,8 +22,26 @@
 ;;; Commentary:
 
 ;; This package adds commands for working with the Git index from
-;; within `diff-mode' which has much better diff manipulation features
-;; than most Git integrations, such as `magit-status'.
+;; within `diff-mode'.
+;;
+;; When splitting up commits and other fine juggling of staged and
+;; unstaged changes, `diff-mode' offers a number of advantages:
+;; specifically `diff-reverse-direction' and `diff-split-hunk' are
+;; extremely useful for teasing apart changes.
+;;
+;; The major commands are: `diff-git-hunk-stage' which takes the hunk
+;; under the point and stages it in the index; `diff-git-diff-staged'
+;; which makes a new diff buffer showing the staged changes in the
+;; file; and `diff-git-diff-unstaged' which shows the unstaged
+;; changes.
+;;
+;; The staged and unstaged diff buffers are refreshed when
+;; `diff-git-hunk-stage' is run, or when the file they are tracking is
+;; saved.
+;;
+;; This extension was written and tested on GNU Emacs 23.0.91.1. To
+;; use this extension you will require magit 0.7 and vc-git (included
+;; with emacs)
 
 ;;; History:
 
